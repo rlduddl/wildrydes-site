5d3151c300d093ff0c2d052e4b6b1637d33815ec
GiYoung <rlduddl5519@naver.com>
2026-04-14 18:14:38 +0900

    daf

--- js/config.js
+++ js/config.js
@@ -5,6 +5,6 @@ window._config = {
         region: 'ap-northeast-2' // e.g. us-east-2
     },
     api: {
-        invokeUrl: 'https://xt7f238qwg.execute-api.ap-northeast-2.amazonaws.com/lasttest' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
+        invokeUrl: 'https://dt1sz76z74.execute-api.ap-northeast-2.amazonaws.com/afasd' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
     }
 };
