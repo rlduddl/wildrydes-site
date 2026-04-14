c1859f67486f6e5ceb8ff796aa5ac99d21e7f410
GiYoung <rlduddl5519@naver.com>
2026-04-14 18:01:45 +0900

    v5

--- js/config.js
+++ js/config.js
@@ -5,6 +5,6 @@ window._config = {
         region: 'ap-northeast-2' // e.g. us-east-2
     },
     api: {
-        invokeUrl: 'https://xt7f238qwg.execute-api.ap-northeast-2.amazonaws.com/prod' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
+        invokeUrl: 'https://xt7f238qwg.execute-api.ap-northeast-2.amazonaws.com/te231' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
     }
 };
